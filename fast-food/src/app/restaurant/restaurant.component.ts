8eb9432fd9d5c326d3b26ea32d0ce52ec9625387
114375-ARROYO-NICOLAS <114375@tecnicatura.frc.utn.edu.ar>
2024-09-20 12:03:35 -0300

    comit

--- fast-food/src/app/restaurant/restaurant.component.ts
+++ fast-food/src/app/restaurant/restaurant.component.ts
@@ -1,15 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { PointOfShellComponent } from '../point-of-shell/point-of-shell.component';
 import { DeliveryPointComponent } from '../delivery-point/delivery-point.component';
 import { KitchenComponent } from '../kitchen/kitchen.component';
+import { PedidoService } from '../pedido.service';
+import { Pedido } from '../pedido';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-restaurant',
   standalone: true,
-  imports: [PointOfShellComponent, DeliveryPointComponent, KitchenComponent],
+  imports: [PointOfShellComponent, DeliveryPointComponent, KitchenComponent,CommonModule],
   templateUrl: './restaurant.component.html',
   styleUrl: './restaurant.component.css'
 })
 export class RestaurantComponent {
 
+  private pedidoService = inject(PedidoService)
+
+  lstPedidos: Pedido[] = [];
+
+  getPedidos() {
+    this.lstPedidos = this.pedidoService.getPedidos();
+  }
+
+  ngOnInit() {
+    this.getPedidos();
+  }
 }
